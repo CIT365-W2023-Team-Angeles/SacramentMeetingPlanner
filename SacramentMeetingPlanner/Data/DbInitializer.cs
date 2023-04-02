456df2be9acab0c6c6259a21e3daf70266c0c983
JulieAntezana <jantezana@byui.edu>
2023-04-02 07:00:23 -0500

    update seed database for meetings

--- SacramentMeetingPlanner/Data/DbInitializer.cs
+++ SacramentMeetingPlanner/Data/DbInitializer.cs
@@ -16,13 +16,26 @@ namespace SacramentMeetingPlanner.Data
             var speakers = new Speaker[]
             {
                 new Speaker{Name="Alexander Carson",Topic="Faith"},
+                new Speaker{Name="Patrick Carson",Topic="Faith"},
+                new Speaker{Name="MaryJane Carson",Topic="Faith"},
                 new Speaker{Name="Meredith Alonso",Topic="Repentance"},
+                new Speaker{Name="Kathy Henderson",Topic="Repentance"},
+                new Speaker{Name="Ralph Henderson",Topic="Repentance"},
                 new Speaker{Name="Arturo Anand",Topic="Baptism"},
+                new Speaker{Name="Elizabeth Birch",Topic="Baptism"},
+                new Speaker{Name="Phil Black",Topic="Baptism"},
+                new Speaker{Name="Philomena Barzdukas",Topic="The Gift of the Holy Ghost"},
+                new Speaker{Name="Cindy Barzdukas",Topic="The Gift of the Holy Ghost"},
                 new Speaker{Name="Gytis Barzdukas",Topic="The Gift of the Holy Ghost"},
                 new Speaker{Name="Yan Li",Topic="The Priesthood"},
+                new Speaker{Name="Linda Rideout",Topic="The Priesthood"},
+                new Speaker{Name="Daniel Drips",Topic="The Priesthood"},
                 new Speaker{Name="Peggy Justice",Topic="The Plan of Salvation"},
+                new Speaker{Name="Paul Johnson",Topic="The Plan of Salvation"},
+                new Speaker{Name="Fernando Atilano",Topic="The Plan of Salvation"},
                 new Speaker{Name="Laura Norman",Topic="Jesus, Our Savior and Redeemer"},
-                new Speaker{Name="Nino Olivetto",Topic="Jesus is Risen"}
+                new Speaker{Name="Nino Olivetto",Topic="Jesus is Risen"},
+                new Speaker{Name="George Hathaway",Topic="Jesus is Risen"}
             };
 
             context.Speakers.AddRange(speakers);
@@ -30,13 +43,13 @@ namespace SacramentMeetingPlanner.Data
 
             var meetings = new Meeting[]
             {
-                new Meeting{MeetingDate=DateTime.Parse("2023-02-26"),Conducting="Bishop Clifford Duke", NumSpeakers=0},
-                new Meeting{MeetingDate=DateTime.Parse("2023-03-5"),Conducting="Ethan Arredando, 1st Counselor", NumSpeakers=0},
-                new Meeting{MeetingDate=DateTime.Parse("2023-03-12"),Conducting="Jim Elliot, 2nd Counselor", NumSpeakers=0},
-                new Meeting{MeetingDate=DateTime.Parse("2023-03-19"),Conducting="Bishop Clifford Duke", NumSpeakers=0},
-                new Meeting{MeetingDate=DateTime.Parse("2023-03-26"),Conducting="Ethan Arredando, 1st Counselor", NumSpeakers=0},
-                new Meeting{MeetingDate=DateTime.Parse("2023-04-9"),Conducting="Jim Elliot, 2nd Counselor", NumSpeakers=0},
-                new Meeting{MeetingDate=DateTime.Parse("2023-04-16"),Conducting="Bishop Clifford Duke", NumSpeakers=0}
+                new Meeting{MeetingDate=DateTime.Parse("2023-02-26"),Conducting="Bishop Clifford Duke", NumSpeakers=3},
+                new Meeting{MeetingDate=DateTime.Parse("2023-03-5"),Conducting="Ethan Arredando, 1st Counselor", NumSpeakers=3},
+                new Meeting{MeetingDate=DateTime.Parse("2023-03-12"),Conducting="Jim Elliot, 2nd Counselor", NumSpeakers=3},
+                new Meeting{MeetingDate=DateTime.Parse("2023-03-19"),Conducting="Bishop Clifford Duke", NumSpeakers=3},
+                new Meeting{MeetingDate=DateTime.Parse("2023-03-26"),Conducting="Ethan Arredando, 1st Counselor", NumSpeakers=3},
+                new Meeting{MeetingDate=DateTime.Parse("2023-04-9"),Conducting="Jim Elliot, 2nd Counselor", NumSpeakers=3},
+                new Meeting{MeetingDate=DateTime.Parse("2023-04-16"),Conducting="Bishop Clifford Duke", NumSpeakers=3}
             };
 
             context.Meetings.AddRange(meetings);
@@ -64,13 +77,7 @@ namespace SacramentMeetingPlanner.Data
                 new Assignments{SpeakerID=18,MeetingID=6},
                 new Assignments{SpeakerID=19,MeetingID=7},
                 new Assignments{SpeakerID=20,MeetingID=7},
-                new Assignments{SpeakerID=21,MeetingID=7},
-                new Assignments{SpeakerID=22,MeetingID=8},
-                new Assignments{SpeakerID=23,MeetingID=8},
-                new Assignments{SpeakerID=24,MeetingID=8},
-                new Assignments{SpeakerID=25,MeetingID=9},
-                new Assignments{SpeakerID=26,MeetingID=9},
-                new Assignments{SpeakerID=27,MeetingID=9},
+                new Assignments{SpeakerID=21,MeetingID=7}
             };
 
             context.Assignments.AddRange(assignments);
