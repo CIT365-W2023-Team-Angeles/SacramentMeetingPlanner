f41b7b00a1f18591ff2d3e5a89eb20a412b7e07b
JulieAntezana <jantezana@byui.edu>
2023-04-03 05:08:48 -0500

    update Meeting Delete

--- SacramentMeetingPlanner/Pages/Meetings/Delete.cshtml.cs
+++ SacramentMeetingPlanner/Pages/Meetings/Delete.cshtml.cs
@@ -1,63 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using SacramentMeetingPlanner.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using SacramentMeetingPlanner.Data;
-using SacramentMeetingPlanner.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace SacramentMeetingPlanner.Pages.Meetings
 {
     public class DeleteModel : PageModel
     {
         private readonly SacramentMeetingPlanner.Data.ChurchContext _context;
+        private readonly ILogger<DeleteModel> _logger;
 
-        public DeleteModel(SacramentMeetingPlanner.Data.ChurchContext context)
+        public DeleteModel(SacramentMeetingPlanner.Data.ChurchContext context,
+                           ILogger<DeleteModel> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         [BindProperty]
-      public Meeting Meeting { get; set; }
+        public Meeting Meeting { get; set; }
+        public string ErrorMessage { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int? id, bool? saveChangesError = false)
         {
-            if (id == null || _context.Meetings == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var meeting = await _context.Meetings.FirstOrDefaultAsync(m => m.MeetingID == id);
+            Meeting = await _context.Meetings
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.MeetingID == id);
 
-            if (meeting == null)
+            if (Meeting == null)
             {
                 return NotFound();
             }
-            else 
+
+            if (saveChangesError.GetValueOrDefault())
             {
-                Meeting = meeting;
+                ErrorMessage = String.Format("Delete {ID} failed. Try again", id);
             }
+
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(int? id)
         {
-            if (id == null || _context.Meetings == null)
+            if (id == null)
             {
                 return NotFound();
             }
+
             var meeting = await _context.Meetings.FindAsync(id);
 
-            if (meeting != null)
+            if (meeting == null)
             {
-                Meeting = meeting;
-                _context.Meetings.Remove(Meeting);
-                await _context.SaveChangesAsync();
+                return NotFound();
             }
 
-            return RedirectToPage("./Index");
+            try
+            {
+                _context.Meetings.Remove(meeting);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            catch (DbUpdateException ex)
+            {
+                _logger.LogError(ex,ErrorMessage);
+
+                return RedirectToAction("./Delete",
+                                     new { id, saveChangesError = true });
+            }
         }
     }
 }
